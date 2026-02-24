cc7730e588e63b8a19f309cf096bc4295f3beb3b
Narayan <kumarnarayan99055@gmail.com>
2026-02-25 02:13:25 +0530

    Updated API base URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",  // 👈 VERY IMPORTANT
+ baseURL: "https://task-management-system-7rhy.onrender.com/api"
 });
 
 // Token auto attach
